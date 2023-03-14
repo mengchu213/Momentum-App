427108d2d6733dd738430a0eacd96d43733e1480
Jose Infante <joseinfa1022@gmail.com>
2023-03-14 20:21:28 +0800

    2nd commit

--- dist/background.js
+++ dist/background.js
@@ -1,15 +1,3 @@
-// const images = ["0.jpg", "1.jpg", "2.jpg", "3.jpg", "4.jpg"];
-// const chosenImage = images[Math.floor(Math.random() * images.length)];
-// const bgImage = document.createElement("img");
-
-// bgImage.src = `imgs/${chosenImage}`;
-
-// //document.body.appendChild(bgImage);
-// document.body.style.backgroundImage =
-// document.body.style.backgroundImage =
-//   "linear-gradient(rgba(0, 0, 0, 0.4), rgba(0, 0, 0, 0.4)), url('" +
-//   bgImage.src +
-//   "')";
 
 // array of image file names
 const images = ["0.jpg", "1.jpg", "2.jpg", "3.jpg", "4.jpg"];
